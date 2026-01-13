2c73ceb6791e20bed3313428f6fdec5e0f50cd38
venusian6 <vivektheviperrockss@gmail.com>
2026-01-14 00:10:42 +0530

    added encrypt and decrypt for ssh key

---
+++ api/shared/crypto.js
@@ -0,0 +1,36 @@
+import crypto from "crypto";
+
+import "dotenv/config";
+
+const algorithm = "aes-256-cbc";
+
+const secret = process.env.MASTER_KEY;
+
+//fixed length key
+
+const key = crypto.createHash("sha256").update(secret).digest();
+
+export function encrypt(text) {
+  const iv = crypto.randomBytes(16);
+
+  const cipher = crypto.createCipheriv(algorithm, key, iv);
+
+  let encrypted = cipher.update(text);
+  encrypted = Buffer.concat([encrypted, cipher.final()]);
+
+  return iv.toString("hex") + ":" + encrypted.toString("hex");
+}
+
+export function decrypt(encryptedText) {
+  const parts = encryptedText.split(":");
+
+  const iv = Buffer.from(parts[0], "hex");
+  const encrypted = Buffer.from(parts[1], "hex");
+
+  const decipher = crypto.createDecipheriv(algorithm, key, iv);
+
+  let decrypted = decipher.update(encrypted);
+  decrypted = Buffer.concat([decrypted, decipher.final()]);
+
+  return decrypted.toString();
+}
