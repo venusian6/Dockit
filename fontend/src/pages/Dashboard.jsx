28e882f777b291af074cc9ccae38f51a30481261
venusian6 <vivektheviperrockss@gmail.com>
2026-01-14 22:07:13 +0530

    added worker file

--- fontend/src/pages/Dashboard.jsx
+++ fontend/src/pages/Dashboard.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import SshKeys from "./SshKey";
+import Servers from "./Servers";
 
 export default function Dashboard() {
   const [page, setPage] = useState("ssh");
