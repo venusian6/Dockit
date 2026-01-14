8898244b040272dbcbfaa8bda02ef66ec37f301e
venusian6 <vivektheviperrockss@gmail.com>
2026-01-14 21:56:20 +0530

    added worker file

--- api/api/sshKeyRoutes.js
+++ api/api/sshKeyRoutes.js
@@ -5,31 +5,57 @@ import { authGuard } from "./authGuard.js";
 
 const router = express.Router();
 
-router.post("/", authGuard, async (req, res) => {
-  const { public_key, private_key } = req.body;
-  console.log("REQ BODY:", req.body);
+// router.post("/", authGuard, async (req, res) => {
+//   const { public_key, private_key } = req.body;
+//   console.log("REQ BODY:", req.body);
 
-  const userId = req.user.userId;
-  if (!public_key || !private_key) {
+//   const userId = req.user.userId;
+//   if (!public_key || !private_key) {
+//     return res.status(400).json({ error: "All fields required" });
+//   }
+
+//   try {
+//     const encryptedPrivateKey = encrypt(private_key);
+//     const result = await db.query(
+//       `INSERT INTO ssh_keys (user_id,public_key,encrypted_private_key)
+//         VALUES ($1,$2,$3)
+//         RETURNING id, public_key`,
+//       [userId, public_key, encryptedPrivateKey]
+//     );
+//     res.json({
+//       id: result.rows[0].id,
+//       public_key: result.rows[0].public_key,
+//     });
+//   } catch (error) {
+//     console.error(err);
+//     res.status(500).json({ error: "Failed to save SSH key" });
+//   }
+// });
+router.post("/", authGuard, async (req, res) => {
+  const { public_key, private_key_b64 } = req.body;
+
+  // Debug (temporary)
+  console.log("SSH KEY BODY:", req.body);
+
+  if (!public_key || !private_key_b64) {
     return res.status(400).json({ error: "All fields required" });
   }
 
-  try {
-    const encryptedPrivateKey = encrypt(private_key);
-    const result = await db.query(
-      `INSERT INTO ssh_keys (user_id,public_key,encrypted_private_key)
-        VALUES ($1,$2,$3)
-        RETURNING id, public_key`,
-      [userId, public_key, encryptedPrivateKey]
-    );
-    res.json({
-      id: result.rows[0].id,
-      public_key: result.rows[0].public_key,
-    });
-  } catch (error) {
-    console.error(err);
-    res.status(500).json({ error: "Failed to save SSH key" });
-  }
+  // decode base64
+  const private_key = Buffer.from(private_key_b64, "base64").toString("utf8");
+
+  const encryptedPrivateKey = encrypt(private_key);
+
+  const result = await db.query(
+    `
+    INSERT INTO ssh_keys (user_id, public_key, encrypted_private_key)
+    VALUES ($1, $2, $3)
+    RETURNING id
+    `,
+    [req.user.userId, public_key.trim(), encryptedPrivateKey]
+  );
+
+  res.json({ id: result.rows[0].id });
 });
 
 router.get("/", authGuard, async (req, res) => {
