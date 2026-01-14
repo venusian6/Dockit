26bbfa24e5223fe15fb8b4861ef8b428701ff36f
venusian6 <vivektheviperrockss@gmail.com>
2026-01-14 15:59:07 +0530

    added worker file

--- api/worker/deploy.js
+++ api/worker/deploy.js
@@ -1,6 +1,5 @@
 export function buildDeployCommand() {
-  return `
-
+  return `echo "HELLO FROM SERVER"
 docker ps > /dev/null 2>&1 || exit 1
 echo "docker is installed"
 `;
--- api/worker/ssh.js
+++ api/worker/ssh.js
@@ -2,12 +2,13 @@ import { exec } from "child_process";
 
 export function runSSHCommand({ host, ssh_user, keyPath, command }) {
   return new Promise((resolve, reject) => {
-    const sshCmd = `
-      ssh -i ${keyPath}
-      -o StrictHostKeyChecking=no
-      ${ssh_user}@${host}
-      "${command}"
-    `;
+    const sshCmd =
+      `ssh -i ${keyPath} ` +
+      `-o StrictHostKeyChecking=no ` +
+      `${ssh_user}@${host} ` +
+      `"${command}"`;
+
+    console.log("🧪 SSH CMD:", sshCmd);
 
     exec(sshCmd, (error, stdout, stderr) => {
       if (error) {
