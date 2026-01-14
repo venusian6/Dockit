f90e9d2aebe5fd2f6c7a471d6a443538acfe4320
venusian6 <vivektheviperrockss@gmail.com>
2026-01-14 15:55:00 +0530

    added worker file

--- api/worker/loadJobDeploy.js
+++ api/worker/loadJobDeploy.js
@@ -3,19 +3,20 @@ import { db } from "../shared/db.js";
 export async function loadJobDeployData(jobId) {
   const result = await db.query(
     `
-        SELECT
-        j.id AS job_id,
-        s.id AS server_id
-        s.host, s.ssh_user
-        k.encrypted_private_key
-        FROM job j
-        JOIN servers s ON j.server_id = s.id
-        JOIN ssh_keys k ON s.ssh_key_id=k.id
-        WHERE j.id =$1
-
-        `,
+    SELECT
+      j.id AS job_id,
+      s.id AS server_id,
+      s.host,
+      s.ssh_user,
+      k.encrypted_private_key
+    FROM jobs j
+    JOIN servers s ON j.server_id = s.id
+    JOIN ssh_keys k ON s.ssh_key_id = k.id
+    WHERE j.id = $1
+    `,
     [jobId]
   );
+  console.log("🧪 SQL QUERY:\n", sql);
   if (result.rows.length === 0) {
     throw new Error("Invalid job");
   }
